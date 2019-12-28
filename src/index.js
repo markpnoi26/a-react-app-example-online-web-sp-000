e3a4d3039fdbfb23e5f1b21da0c0895e6bc258d2
markpnoi26 <markpnoi26@gmail.com>
2019-12-27 21:09:45 -0800

    Done.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Appp from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Appp />, document.getElementById('root'));
